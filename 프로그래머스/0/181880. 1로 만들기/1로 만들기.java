63b46063e13ddbdc7e4f5bca7aaa01c13790a45e
yoon geon <ygunkun1015@gmail.com>
2025-04-19 12:20:35 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        for(int i : num_list){
+            while(i != 1){
+                if(i % 2 == 0){
+                    i /= 2;
+                }
+                else{
+                    i = (i-1)/2;
+                }
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
