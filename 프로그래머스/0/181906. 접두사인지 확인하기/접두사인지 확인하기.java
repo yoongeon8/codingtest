50464381c19c872169b6e5a64832b7c754c96104
yoon geon <ygunkun1015@gmail.com>
2025-08-21 08:59:03 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        String arr[] = new String[my_string.length()];
+        for(int i = 0; i < my_string.length(); i++){
+            arr[i] = my_string.substring(0, i+1);
+        }
+        for(String a : arr){
+            if(a.equals(is_prefix)){
+                answer = 1;
+            }
+        }
+        return answer;
+    }
+}
