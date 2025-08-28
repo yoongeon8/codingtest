260a33c20c234ac13ff93684dcc41b9cfdeffcb5
yoon geon <ygunkun1015@gmail.com>
2025-08-28 10:16:02 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String a = Integer.toString(n);
+        for(int i = 0; i < a.length(); i++){
+            answer += Character.getNumericValue(a.charAt(i));
+        }
+        return answer;
+    }
+}
