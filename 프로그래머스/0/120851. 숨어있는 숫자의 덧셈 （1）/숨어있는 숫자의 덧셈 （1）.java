33105e900f2634bf755f9dbe70883159eaaa2b0f
yoon geon <ygunkun1015@gmail.com>
2025-09-01 10:05:27 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.17 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] arr = my_string.replaceAll("[a-z,A-Z]","").split("");
+        for(int i = 0; i < arr.length; i++){
+            answer += Integer.parseInt(arr[i]);
+        }
+        return answer;
+    }
+}
