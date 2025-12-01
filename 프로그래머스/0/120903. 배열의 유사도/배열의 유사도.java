6863b8e271a07f194f71b1553f85d0823ebef6cf
yoon geon <ygunkun1015@gmail.com>
2025-12-01 16:09:03 +0900

    [level 0] Title: 배열의 유사도, Time: 0.09 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for(int i = 0; i < s1.length; i++){
+            if(Arrays.asList(s2).contains(s1[i])){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
